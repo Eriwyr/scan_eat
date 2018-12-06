be6c6286e415cacb7d8d82c6408e55809ceff4f0
maxencebernier <maxencebernier@hotmail.fr>
2018-12-06 17:27:30 +0100

    database script

---
+++ database.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le :  jeu. 06 déc. 2018 à 14:58
+-- Version du serveur :  5.6.38
+-- Version de PHP :  7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `scan_eat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient`
+--
+
+CREATE TABLE `ingredient` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recipe`
+--
+
+CREATE TABLE `recipe` (
+  `id` int(11) NOT NULL,
+  `title` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recipe_ingredient`
+--
+
+CREATE TABLE `recipe_ingredient` (
+  `id_recipe` int(11) NOT NULL,
+  `id_ingredient` int(11) NOT NULL,
+  `quantity` float NOT NULL,
+  `unit` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `ingredient`
+--
+ALTER TABLE `ingredient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `recipe`
+--
+ALTER TABLE `recipe`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `recipe_ingredient`
+--
+ALTER TABLE `recipe_ingredient`
+  ADD PRIMARY KEY (`id_recipe`,`id_ingredient`);
