80a27543a0f07eb23bf804a46497c856c417955b
Valentin chabrier <kristoffer@brastrup.com>
2018-12-11 17:16:24 +0100

    recipe rest 1/3 finish

--- scan_eat_api/src/main/java/com/codev/scan_eat_api/controller/RecipeController.java
+++ scan_eat_api/src/main/java/com/codev/scan_eat_api/controller/RecipeController.java
@@ -3,13 +3,17 @@ package com.codev.scan_eat_api.controller;
 import com.codev.scan_eat_api.entities.Recipe;
 import com.codev.scan_eat_api.repository.RecipeDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/")
-public class RecipeController {
+public class RecipeController implements ErrorController {
 
     final RecipeDao recipeDao;
 
@@ -22,4 +26,28 @@ public class RecipeController {
     private Iterable<Recipe> allRecipes() {
         return recipeDao.findAll();
     }
+
+
+    @GetMapping(path = "/recipes/{id}")
+    //@RequestMapping(value = "/recipes", params = "id", method = GET)
+    private ResponseEntity<Recipe> recipe(@PathVariable int id) {
+        Optional<Recipe> recipe = recipeDao.findById(id);
+        if(recipe.isPresent()) {
+            return ResponseEntity.accepted().body(recipe.get());
+        } else {
+            return ResponseEntity.notFound().build();
+            //throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe Not Found");
+        }
+        //return recipeDao.findAll();
+    }
+
+    private final static String PATH = "/error";
+    @Override
+    @RequestMapping(PATH)
+    @ResponseBody
+    public String getErrorPath() {
+        // TODO Auto-generated method stub
+        return "No Mapping Found";
+    }
+
 }
